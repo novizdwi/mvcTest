7f376787766296956f2a638314e68586bbe9b617
developer.performate <developer@performate.id>
2020-07-26 15:53:03 +0700

    init

---
+++ sTest/Controllers/DepartmentController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Collections;
+using sTest.Models;
+
+namespace sTest.Controllers
+{
+    public class DepartmentController : Controller
+    {
+
+        public ActionResult DepartmentList() {
+            var Department = new department();
+            ViewBag.Heading = "Department List";
+            
+            return View(Department);
+        }
+
+        // GET: Department
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ sTestDataLib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace sTestDataLib
+{
+    public class Class1
+    {
+    }
+}
